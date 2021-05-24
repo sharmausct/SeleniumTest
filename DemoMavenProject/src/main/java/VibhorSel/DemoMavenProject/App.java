0f245640f1eaa1594f04b417d4e4ed87d3c9a319
sharm <sharm@192.168.2.22>
2021-05-24 00:17:52 -0400

    myfirstcomit

---
+++ DemoMavenProject/src/main/java/VibhorSel/DemoMavenProject/App.java
@@ -0,0 +1,13 @@
+package VibhorSel.DemoMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
